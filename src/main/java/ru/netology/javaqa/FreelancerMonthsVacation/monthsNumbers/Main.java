fc96dee85a441d19a3d91962098b86175cfe09e1
ZhekaSPB <23121986@list.ru>
2023-06-01 00:59:50 +0300

    editing conditions

--- src/main/java/ru/netology/javaqa/FreelancerMonthsVacation/monthsNumbers/Main.java
+++ src/main/java/ru/netology/javaqa/FreelancerMonthsVacation/monthsNumbers/Main.java
@@ -1,12 +1,12 @@
 package ru.netology.javaqa.FreelancerMonthsVacation.monthsNumbers;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         MonthsNumberService calculate = new MonthsNumberService();
 
         int count = calculate.calculate(10000, 3000, 20000);
-        System.out.println("Months to be vacation =" + count);
+        System.out.println("Months to be vacation = " + count);
 
     }
 
